b4598b878386473ae317336889c7960c7043deff
GandhyCrush <gandhy.garcia@epn.edu.ec>
2024-09-24 15:11:54 -0500

    chore: Change code structure (frontend-backend)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useQuery } from '@tanstack/react-query'
+import { get } from './services/api'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [queryActive, setQueryActive] = useState(false)
+
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ['todos'],
+    queryFn: async () => {
+      const response = await get('/get_entries')
+
+      console.log(response.data)
+      setQueryActive(false)
+      
+
+      return response.data
+      
+    },
+    //retryOnMount: false,
+    enabled: queryActive,
+  })
+
+  const handleTest = async () => {
+    console.log('Test')
+    setQueryActive(true)
+  }
 
   return (
     <>
@@ -16,7 +40,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Web Crawler</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -24,6 +48,11 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+      </div>
+      <div className='card'>
+        <button onClick={handleTest}>
+          See results
+        </button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
